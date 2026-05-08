c69f94eeef4330b07c5c621deae13f12475fb43e
kitsunesensei <senpai@ktsu.me>
2026-05-08 14:57:33 +0200

    ci: automated releases

---
+++ release.config.mjs
@@ -0,0 +1,15 @@
+import { releaseAssets } from "./scripts/release-assets.ts";
+
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: await releaseAssets(),
+      },
+    ],
+  ],
+};
